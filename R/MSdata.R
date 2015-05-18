16ffc239268990fbabc0aeaa5a15b959a79e78f2
wilsontom <wilsontom89@gmail.com>
2015-05-18 22:13:37 +0100

    overhaul

--- R/MSdata.R
+++
@@ -1,7 +0,0 @@
-MSdata <- function(...)
-{
-  tmp  <- data(package = "MSdata")$results
-  
-  cat(paste(tmp[,"Item"], tmp[,"Title"], sep = ": "))
-  
-}
--- R/allClasses.R
+++
@@ -1,7 +0,0 @@
-setClass(Class = "exampleData",
-        representation = representation(
-        env = "environment",
-        cls = "vector",
-        negative = "data.frame",
-        positive = "data.frame")
-  )
