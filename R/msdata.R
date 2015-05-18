9ae0908e718778588d137f9b5c1f85fa1989b4e7
wilsontom <wilsontom89@gmail.com>
2015-05-18 22:13:53 +0100

    overhaul

---
+++ R/msdata.R
@@ -0,0 +1,51 @@
+#' Large High Resolution FIE-MS data set
+#' @name sin132HRMS
+#' @description \code{sin132HRMS} is a FIE-HRMS data sets contaning 1576 observations across 
+#' 132 classes.   
+#' \itemize{
+#'    \item pos. positive ionisation mode wiith 3079 variables
+#'    \item neg. negative ionisation mode wiith 1452 variables
+#'    \item cls. vector of 132 unique class lables
+#'    
+#'  }
+#' @docType data
+#' @usage data(sin132HRMS)
+#' @author Tom Wilson
+#'
+NULL
+
+
+#' Large LTQ FIE-MS data set
+#' @name sin132LTQ
+#' @description \code{sin132LTQ} is a FIE-MS data sets contaning 1320 observations across 
+#' 132 classes.   
+#' \itemize{
+#'    \item pos. positive ionisation mode wiith 742 variables
+#'    \item neg. negative ionisation mode wiith 688 variables
+#'    \item cls. vector of 132 unique class lables
+#'    
+#'  }
+#' @docType data
+#' @usage data(sin132LTQ)
+#' @author Tom Wilson
+#'
+NULL
+
+
+
+
+#' Binary FIE-HRMS - (1)
+#' @name pw1HRMS
+#' @description \code{pw1HRMS} is a FIE-HRMMS data sets contaning of 24 observations across 2 classes. The two classes are 
+#' two distincit accessions from a large scaling profiling experiment.
+#' \itemize{
+#'    \item pos. positive ionisation mode wiith 3079 variables
+#'    \item neg. negative ionisation mode wiith 1422 variables
+#'    \item cls. vector of 2 unique class lables11
+#'    
+#'  }
+#' @docType data
+#' @usage data(pw1HRMS)
+#' @author Tom Wilson
+#'
+NULL
