6127c23e3a2e848dfb7acbe26101a60f464c49ab
Tom Wilson <wilsontom89@gmail.com>
2015-10-06 14:46:11 +0100

    update

--- R/msdata.R
+++ R/msdata.R
@@ -1,10 +1,10 @@
-#' Large High Resolution FIE-MS data set
+#' Large High Resolution FIE-MS data set (processed)
 #' @name sin132HRMS
 #' @format List object containing three elements
 #' 	\itemize{
 #'		\item pos positive ionisation mode data frame (3079 variables)
 #'		\item neg negative ionisation mode data frame (1452 variables)
-#'		\item cls vector of class labels
+#'		\item cls vector of class labels (N = 132)
 #'	}
 #' @docType data
 #' @usage data(sin132HRMS)
@@ -12,38 +12,58 @@
 "sin132HRMS"
 
 
-#' Large LTQ FIE-MS data set
+#' Large LTQ FIE-MS data set (processed)
 #' @name sin132LTQ
 #' @format List object containing three elements
 #' 	\itemize{
 #'		\item pos positive ionisation mode data frame (742 variables)
 #'		\item neg negative ionisation mode data frame (688 variables)
-#'		\item cls vector of class labels
+#'		\item cls vector of class labels (N = 132)
 #'	}
 #' @docType data
 #' @usage data(sin132LTQ)
 #' @author Tom Wilson
 "sin132LTQ"
 
-#' Binary (N = 2) High Resolution FIE-MS data set
+#' Binary High Resolution FIE-MS data set (processed)
 #' @name pw1HRMS
 #' @format List object containing three elements
 #' 	\itemize{
 #'		\item pos positive ionisation mode data frame (3079 variables)
 #'		\item neg negative ionisation mode data frame (1452 variables)
-#'		\item cls vector of class labels
+#'		\item cls vector of class labels (N = 2)
 #'	}
 #' @docType data
 #' @usage data(pw1HRMS)
 #' @author Tom Wilson
 "pw1HRMS"
 
-
-#' Parsed FIE-HRMS mzXML
-#' @name FIEHRMSraw
-#' @format List object containing 2 elements; \code{positive} and \code{negative}, each with 24 sub-elements. The sub-elements 
-#' of each ionisation mode are the raw parsed output of \code{mzXML} files using \code{FIEtools::readFIE}
+#' Large LTQ FIE-MS data set (raw)
+#' @name sin132LTQ
+#' @format List object containing three elements
+#' 	\itemize{
+#'		\item pos positive ionisation mode data frame (1822 variables)
+#'		\item neg negative ionisation mode data frame (1813 variables)
+#'		\item cls vector of class labels (N = 132)
+#'	}
 #' @docType data
-#' @usage data(FIEHRMSraw)
+#' @usage data(sin132LTQ_raw)
 #' @author Tom Wilson
-"FIEHRMSraw"
+"sin132LTQ_raw"
+
+
+#' Single ionisaiton mode of abr1 FIE-MS data (raw)
+#' @name abr
+#' @format List object containing two elements
+#' 	\itemize{
+#'		\item neg negative ionisation mode data frame (2000 variables)
+#'		\item cls vector of class labels (N = 6)
+#'	}
+#' @docType data
+#' @usage data(abr)
+#' @author Tom Wilson
+"abr"
+
+
+
+
